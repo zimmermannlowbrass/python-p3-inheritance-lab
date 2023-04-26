a3813934bc8007c45d1c1f7e6d41efb744a5eced
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-04-26 15:59:51 -0500

    completed labwork

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,11 @@
 from user import User
 
 class Student(User):
+
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.knowledge = []
     
-    def learn(self):
-        pass
+    def learn(self, information):
+        self.knowledge.append(information)
--- lib/teacher.py
+++ lib/teacher.py
@@ -6,5 +6,19 @@ import random
 
 class Teacher(User):
 
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.knowledge = [
+        "str is a data type in Python",
+        "programming is hard, but it's worth it",
+        "JavaScript async web request",
+        "Python function call definition",
+        "object-oriented teacher instance",
+        "programming computers hacking learning terminal",
+        "pipenv install pipenv shell",
+        "pytest -x flag to fail fast",
+    ]
+
     def teach(self):
-        pass
+        return self.knowledge[random.randint(0, len(self.knowledge) - 1)]
